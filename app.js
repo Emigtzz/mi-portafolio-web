357d1fa310e9e653292f998cad74a95705f1f9ac
Emigtzz <emilianogutierrezluna135@gmail.com>
2026-06-05 18:51:03 -0600

    Primer esqueleto del portafolio

---
+++ app.js
@@ -0,0 +1,4 @@
+// Este código se ejecuta cuando la página carga en el navegador
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("¡El archivo JavaScript se ha conectado correctamente!");
+});
